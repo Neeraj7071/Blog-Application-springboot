40f4bbe86d4c8c683be1c5d849f7b757281a19ee
Neeraj Khajuriya <neerajkhajuriya7071@gmail.com>
2023-01-04 10:16:58 +0530

    update security

--- src/main/java/com/neebal/security/service/MyUserDetailsService.java
+++ src/main/java/com/neebal/security/service/MyUserDetailsService.java
@@ -1,19 +1,19 @@
-//package com.neebal.security.service;
-//
-//import java.util.ArrayList;
-//
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class MyUserDetailsService implements UserDetailsService{
-//
-//	@Override
-//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		return new User("neeraj","neeraj",new ArrayList<>());
-//	}
-//
-//}
+package com.neebal.security.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new User("neeraj","neeraj",new ArrayList<>());
+	}
+
+}
