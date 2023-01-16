544043c39ed2fae677d008584f05402c5a5a087d
Neeraj Khajuriya <97509661+Neeraj7071@users.noreply.github.com>
2023-01-16 16:19:32 +0530

    Delete src/src/main directory

--- src/src/main/java/com/neebal/repositry/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.neebal.repositry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.neebal.model.Category;
-
-public interface CategoryDao extends JpaRepository<Category,Integer>{
-
-}
--- src/src/main/java/com/neebal/security/filter/IAuthenticationFacade.java
+++
@@ -1,7 +0,0 @@
-package com.neebal.security.filter;
-
-import org.springframework.security.core.Authentication;
-
-public interface IAuthenticationFacade {
-    Authentication getAuthentication();
-}
